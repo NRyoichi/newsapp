c9b24f0ec4fb7f2523492100c9bd348a1ad074d6
NRyoichi <95951809+NRyoichi.noreply.github.com>
2022-06-19 10:04:15 +0900

    レイアウト暫定

--- src/components/weaather/Weather.tsx
+++ src/components/weaather/Weather.tsx
@@ -1,7 +1,48 @@
 import { AxiosError, AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { weatherInstance } from "../../axios";
 import { WEATHER } from "../../type";
+
+const WeatherSection = styled.section`
+  border: 1.2px solid rgba(135, 135, 135, 0.5);
+  border-radius: 15px;
+  padding: 0 20px 0 20px;
+  margin: 30px 20px 0 20px;
+  h1 {
+    font-weight: 1000;
+    font-size: 16px;
+    color: rgb(233, 163, 163);
+    padding: 12px 0;
+    border-bottom: 1px solid rgba(150, 113, 113, 0.5);
+  }
+`;
+const WeatherTop = styled.div`
+  ul {
+    display: flex;
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+    li {
+      list-style-type: none;
+      p {
+        text-align: center;
+      }
+      .weatherIcon {
+        font-size: 70px;
+      }
+      .weater_temp {
+        margin-bottom: 10px;
+        .weater_temp_max {
+          font-weight: 50px;
+        }
+        .weather_temp_min {
+          font-weight: 100;
+        }
+      }
+    }
+  }
+`;
 
 export const Weather = () => {
   const [weathers, setWeathers] = useState<WEATHER[]>([]);
@@ -24,37 +65,40 @@ export const Weather = () => {
   }, []);
   console.log(weathers);
   return (
-    <section className="Weather">
+    <WeatherSection>
       <h1>Japan</h1>
-      <div>
-        <div className="Weathermain">
-          <div className="Weathertop">
-            <div className="WeatherHeading">
-              {weathers.map((weather, index: number) => (
-                <li key={index}>
-                  <p>{weather.date}</p>
-                  <span>
-                    <img
-                      src={weather.day.condition.icon}
-                      alt={`${weather.date}'sicon`}
-                      width={41}
-                      height={41}
-                    />
-                  </span>
-                  <div className="weather_temp">
-                    <p className="weater-temp-max">
-                      {parseInt(weather.day.maxtemp_c.toLocaleString(), 10)}℃
-                    </p>
-                    <p className="weater-temp-min">
-                      {parseInt(weather.day.mintemp_c.toLocaleString(), 10)}℃
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </div>
-          </div>
-        </div>
+      <div className="Weathermain">
+        <WeatherTop>
+          <ul className=" WeatherHeading">
+            {weathers.map((weather, index: number) => (
+              <li key={index}>
+                <p>
+                  {weather.date.substring(6, 7) +
+                    `/` +
+                    weather.date.substring(8, 11)}
+                </p>
+                <span>
+                  <img
+                    src={weather.day.condition.icon}
+                    alt={`${weather.date}'sicon`}
+                    width={41}
+                    height={41}
+                    className="weatherIcon"
+                  />
+                </span>
+                <div className="weather_temp">
+                  <p className="weater-temp-max">
+                    {parseInt(weather.day.maxtemp_c.toLocaleString(), 10)}℃
+                  </p>
+                  <p className="weater-temp-min">
+                    {parseInt(weather.day.mintemp_c.toLocaleString(), 10)}℃
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </WeatherTop>
       </div>
-    </section>
+    </WeatherSection>
   );
 };
